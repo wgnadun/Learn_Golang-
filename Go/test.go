78e198a3845fe823715de13620231a4dc3729605
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-06-05 18:34:29 +0530

    learn strings

--- Go/test.go
+++ Go/test.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main(){
+     
+	var name string = "nadun"
+	var vilage  = "matara"
+	    hobby:= "coding"
+
+	fmt.Println("Hello " + name)
+	fmt.Println("Im from " + vilage)
+	fmt.Println("my hobby is" + hobby)
+   
 	fmt.Println("Hello, World!")
 }
