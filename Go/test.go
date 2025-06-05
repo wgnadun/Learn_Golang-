e45346d8dd42c1916a5eba1e236c44f5b7cdfc15
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-06-05 20:05:23 +0530

    learn arrays and slices

--- Go/test.go
+++ Go/test.go
@@ -8,9 +8,35 @@ func main(){
 	var vilage  = "matara"
 	    hobby:= "coding"
 
+		var ages = [3]int{10, 20, 30}
+
+	names := [4]string{"Alice", "Bob", "Charlie", "Diana"}
+	names[1] = "luigi"
+
+
 	fmt.Println("Hello " + name)
 	fmt.Println("Im from " + vilage)
-	fmt.Println("my hobby is" + hobby)
+	fmt.Println("my hobby is " + hobby)
    
 	fmt.Println("Hello, World!")
+
+
+
+	fmt.Println(ages, len(ages))
+	fmt.Println(names, len(names))
+
+	var scores =[]int{10, 20, 30}
+	scores[2] = 25
+	scores = append(scores,85)
+
+	fmt.Println(scores, len(scores))
+	////slice ranges
+
+	rangeone := scores[0:2]
+	rangetwo := names[1:3]
+	ramgethee := names[2:]
+
+    fmt.Println(rangeone,len(rangeone))
+    fmt.Println(rangetwo,len(rangetwo))
+    fmt.Println(ramgethee,len(ramgethee))
 }
